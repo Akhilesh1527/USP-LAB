a75b2a8056717a842d2f05908e299313c9ff8ebe
Akhilesh1527 <71307601+Akhilesh1527@users.noreply.github.com>
2021-11-11 13:00:36 +0530

    Create gross.sh

---
+++ LAB-3/gross.sh
@@ -0,0 +1,7 @@
+echo "Computing Gross Salary of an employee"
+echo "Enter the basic Salary :"
+read basic
+DA=`echo " $basic * 0.1 " |bc`
+HRA=`echo " $basic * 0.2 " |bc`
+GS=`echo "$basic+$DA+$HRA" |bc`
+echo "Gross-Salary=$GS"
